ac81855998b3992e93b267c9077ca588a8199026
Aleksandr Razumov <ernado@go-faster.org>
2025-02-22 21:51:41 +0300

    feat: implement cluster support

--- cmd/multiredis/main.go
+++ cmd/multiredis/main.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"net"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/go-faster/errors"
 	"github.com/spf13/cobra"
@@ -21,14 +24,24 @@ type RedisInstance struct {
 	ReplicaOf string `yaml:"replicaOf"`
 }
 
+type RedisCluster struct {
+	StartPort int `yaml:"startPort"`
+	Replicas  int `yaml:"replicas"`
+}
+
 type Config struct {
-	BaseDir   string                   `yaml:"base_dir"`
+	BaseDir   string                   `yaml:"baseDir"`
 	Instances map[string]RedisInstance `yaml:"instances"`
+	Clusters  map[string]RedisCluster  `yaml:"clusters"`
 }
 
 type RedisConfig struct {
 	Bind []string
 	Port int
+
+	ClusterEnabled     bool
+	ClusterConfigFile  string
+	ClusterNodeTimeout int
 
 	ReplicaOfHost string
 	ReplicaOfPort int
@@ -55,6 +68,16 @@ func (r RedisConfig) String() string {
 		b.WriteString(r.ReplicaOfHost)
 		b.WriteString(" ")
 		b.WriteString(fmt.Sprintf("%d", r.ReplicaOfPort))
+		b.WriteString("\n")
+	}
+
+	if r.ClusterEnabled {
+		b.WriteString("cluster-enabled yes\n")
+		b.WriteString("cluster-config-file ")
+		b.WriteString(r.ClusterConfigFile)
+		b.WriteString("\n")
+		b.WriteString("cluster-node-timeout ")
+		b.WriteString(fmt.Sprintf("%d", r.ClusterNodeTimeout))
 		b.WriteString("\n")
 	}
 
@@ -139,6 +162,9 @@ func newRootCommand() *cobra.Command {
 			const host = "127.0.0.1"
 
 			ctx := cobraCommand.Context()
+			ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+			defer cancel()
+
 			g, ctx := errgroup.WithContext(ctx)
 			for name, instance := range config.Instances {
 				g.Go(func() error {
@@ -163,13 +189,84 @@ func newRootCommand() *cobra.Command {
 						return errors.Wrap(err, "write config file")
 					}
 
-					cmd := exec.Command("redis-server", configPath)
+					cmd := exec.CommandContext(ctx, "redis-server", configPath)
 					prefix := fmt.Sprintf("[%s] ", name)
 					cmd.Stdout = newPrefixWriter(mux, prefix)
 					cmd.Stderr = newPrefixWriter(mux, prefix)
 					cmd.Dir = instanceDir
 
 					return cmd.Run()
+				})
+			}
+
+			for clusterName, cluster := range config.Clusters {
+				var nodes []string
+				for i := 0; i < cluster.Replicas; i++ {
+					instanceName := fmt.Sprintf("%s-%d", clusterName, i)
+					instancePort := cluster.StartPort + i
+					instanceAddr := net.JoinHostPort(host, fmt.Sprintf("%d", instancePort))
+					nodes = append(nodes, instanceAddr)
+
+					instanceDir := filepath.Join(config.BaseDir, clusterName, fmt.Sprintf("%d", i))
+					if err := os.MkdirAll(instanceDir, 0700); err != nil {
+						return errors.Wrap(err, "create instance dir")
+					}
+
+					g.Go(func() error {
+						configPath := filepath.Join(instanceDir, "redis.conf")
+						cfg := RedisConfig{
+							Bind:               []string{host},
+							Port:               instancePort,
+							ClusterEnabled:     true,
+							ClusterConfigFile:  "nodes.conf",
+							ClusterNodeTimeout: 5000,
+						}
+						prefix := fmt.Sprintf("[%s] ", instanceName)
+
+						cfgData := []byte(cfg.String())
+						if err := os.WriteFile(configPath, cfgData, 0600); err != nil {
+							return errors.Wrap(err, "write config file")
+						}
+
+						cmd := exec.CommandContext(ctx, "redis-server", configPath)
+						cmd.Stdout = newPrefixWriter(mux, prefix)
+						cmd.Stderr = newPrefixWriter(mux, prefix)
+						cmd.Dir = instanceDir
+
+						if err := cmd.Run(); err != nil {
+							return errors.Wrapf(err, "start redis instance %s", instanceName)
+						}
+
+						return nil
+					})
+				}
+
+				g.Go(func() error {
+					time.Sleep(time.Second)
+
+					cmdArgs := []string{
+						"--cluster",
+						"create",
+					}
+					cmdArgs = append(cmdArgs, nodes...)
+					cmdArgs = append(cmdArgs, "--cluster-replicas", "1")
+					cmdArgs = append(cmdArgs, "--cluster-yes")
+
+					cmd := exec.CommandContext(ctx, "redis-cli", cmdArgs...)
+
+					buf := &bytes.Buffer{}
+					cmd.Stdout = buf
+					cmd.Stderr = buf
+
+					if err := cmd.Run(); err != nil {
+						if strings.Contains(buf.String(), "is not empty") {
+							// Already created.
+							return nil
+						}
+						return errors.Wrap(err, "create redis cluster")
+					}
+
+					return nil
 				})
 			}
 
